0a34e95108f87d6e798e2539a86913d7baf751c4
Yesa <yesasurya01@gmail.com>
2019-04-19 17:24:29 +0700

    Init project

---
+++ ConfigurationListener.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+import subprocess
+from ConfigurationManager import *
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def root():
+    return jsonify("The ConfigurationListener is working.")
+
+
+@app.route("/modify", methods=["POST"])
+def modify():
+    filename = "/app/out/" + request.form["filename"]
+    tfjob_name = request.form["tfjob_name"]
+    tfjob_master_replica = request.form["tfjob_master_replica"]
+    tfjob_master_image = request.form["tfjob_master_image"]
+    tfjob_worker_replica = request.form["tfjob_worker_replica"]
+    tfjob_worker_image = request.form["tfjob_worker_image"]
+    tfjob_ps_replica = request.form["tfjob_ps_replica"]
+    tfjob_ps_image = request.form["tfjob_ps_image"]
+
+    if filename is None or tfjob_name is None or tfjob_master_replica is None or tfjob_master_image is None or \
+            tfjob_worker_replica is None or tfjob_worker_image is None or tfjob_ps_replica is None or \
+            tfjob_ps_image is None:
+        return jsonify("Please complete all arguments needed!")
+
+    config_manager = ConfigurationManager(filename)
+    config_manager.set_tfjob_name(tfjob_name)
+    config_manager.set_tfjob_master_replica(tfjob_master_replica)
+    config_manager.set_tfjob_master_image(tfjob_master_image)
+    config_manager.set_tfjob_worker_replica(tfjob_worker_replica)
+    config_manager.set_tfjob_worker_image(tfjob_worker_image)
+    config_manager.set_tfjob_ps_replica(tfjob_ps_replica)
+    config_manager.set_tfjob_ps_image(tfjob_ps_image)
+    config_manager.generate_config_file()
+
+    return jsonify("Configuration generated and applied.")
---
+++ ConfigurationManager.py
@@ -0,0 +1,124 @@
+class ConfigurationManager:
+    def __init__(self, filename, special_keywords=None):
+        self.file = open(filename, "w")
+        self.special_keywords = ["containers"] if special_keywords is None else special_keywords
+        pass
+
+    def generate_config_file(self):
+        self.__generate_config_file(self.CONFIG, 0, False)
+
+    def set_tfjob_name(self, name):
+        self.CONFIG[5][1] = name
+
+    def set_tfjob_master_image(self, image):
+        self.CONFIG[7][1][1][3][1][1][3] = image
+
+    def set_tfjob_master_replica(self, num):
+        self.CONFIG[7][1][1][1] = num
+
+    def set_tfjob_worker_image(self, image):
+        self.CONFIG[7][1][3][3][1][1][3] = image
+
+    def set_tfjob_worker_replica(self, num):
+        self.CONFIG[7][1][3][1] = num
+
+    def set_tfjob_ps_image(self, image):
+        self.CONFIG[7][1][5][3][1][1][3] = image
+
+    def set_tfjob_ps_replica(self, num):
+        self.CONFIG[7][1][5][1] = num
+
+    def __generate_config_file(self, config, order, is_special_keyword):
+        for index in range(len(config)):
+            # Writing key
+            if index % 2 == 0:
+                for i in range(2 * order):
+                    self.file.write(" ")
+                if is_special_keyword:
+                    self.file.write("- ")
+                    is_special_keyword = False
+                    order = order + 1
+                self.file.write(config[index] + ": ")
+
+            # Writing value
+            else:
+                if isinstance(config[index], list):
+                    self.file.write("\n")
+                    self.__generate_config_file(
+                        config[index], order + 1,
+                        True if config[index - 1] in self.special_keywords else False
+                    )
+                else:
+                    self.file.write(config[index] + "\n")
+
+    CONFIG = [
+        "apiVersion",
+        "kubeflow.org/v1alpha2",
+        "kind",
+        "TFJob",
+        "metadata",
+        [
+            "name",
+            "dist-tf-mnist"
+        ],
+        "spec",
+        [
+            "tfReplicaSpecs",
+            [
+                "Master",
+                [
+                    "replicas",
+                    "1",
+                    "template",
+                    [
+                        "spec",
+                        [
+                            "containers",
+                            [
+                                "name",
+                                "tensorflow",
+                                "image",
+                                "ysyesa/dist-tf-mnist"
+                            ]
+                        ]
+                    ]
+                ],
+                "Worker",
+                [
+                    "replicas",
+                    "1",
+                    "template",
+                    [
+                        "spec",
+                        [
+                            "containers",
+                            [
+                                "name",
+                                "tensorflow",
+                                "image",
+                                "ysyesa/dist-tf-mnist"
+                            ]
+                        ]
+                    ]
+                ],
+                "PS",
+                [
+                    "replicas",
+                    "1",
+                    "template",
+                    [
+                        "spec",
+                        [
+                            "containers",
+                            [
+                                "name",
+                                "tensorflow",
+                                "image",
+                                "ysyesa/dist-tf-mnist"
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
